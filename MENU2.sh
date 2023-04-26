87f0bb5f7e9d5378970ed86911b613d4a816406d
iriaPM <124077511+iriaPM@users.noreply.github.com>
2023-04-26 17:37:20 +0100

    Create MENU2.sh

---
+++ MENU2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PS3="* Bash Commands *"
+options=("List files" "Show Free disk space" "Show system path" "Display command history" "Backup files" "Exit")
+FILENAME=$""
+menu()
+{
+    echo *Please select an option:*
+    select opt in "${options[@]}"
+    do 
+        case $opt in 
+            "List files")
+                echo "this is a list of files"
+                ls;;
+
+            "Show Free disk space")
+                echo "this will free the disk space"
+                df -h;;
+
+            "Show system path")
+                echo "system path:" 
+                $PATH;;
+
+            "Display command history")
+                echo "display command history:"
+                history;;
+
+            "Backup files")
+                echo "backup files:"
+                echo "enter name of directory:"
+                file="$1"
+                read FILENAME
+                    cp *.txt   BackupFolder
+                    ls -l *;;
+
+            "Exit")
+                echo "exiting..."
+                break;;
+            *)
+                echo "please select option from the menu";;
+        esac 
+    done 
+}
+menu
